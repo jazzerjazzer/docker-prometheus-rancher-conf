88d53a92766a88d5c642a9b9427a05696fb550ba
Ryan Schmukler <rschmukler@gmail.com>
2016-03-08 22:26:43 -0500

    wait for rancher-metadata

--- run.sh
+++ run.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+# borrowed from github.com/martinbaillie
+wait_for_rancher_metadata() {
+  counter=0
+  printf '[prometheus-config] waiting for rancher-metadata to become available'
+  until curl --output /dev/null --silent --head --fail http://rancher-metadata; do
+    printf '.'
+    sleep 1
+    counter=$((counter + 1))
+    if [ "$counter" -gt 10 ]; then
+      echo
+      echo "[prometheus-config] rancher-metadata never became available" >&2
+      exit 1
+    fi
+  done
+  echo
+}
+
+echo "[prometheus-config] waiting for rancher-metadata to become available"
+wait_for_rancher_metadata
 echo "[prometheus-config] watching for changes every ${CONFD_INTERVAL}s"
 confd -interval $CONFD_INTERVAL -backend rancher -prefix /2015-12-19
