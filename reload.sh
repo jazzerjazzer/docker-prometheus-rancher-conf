a5577ca20331f7defe1b1435deac9fbb9a63e826
Ryan Schmukler <rschmukler@gmail.com>
2016-03-08 22:17:04 -0500

    only reload if we are authorized

--- reload.sh
+++ reload.sh
@@ -1,3 +1,5 @@
-rancher-compose --url $CATTLE_CONFIG_URL \
-  --access-key $CATTLE_ACCESS_KEY --secret-key $CATTLE_SECRET_KEY \
-  -p $RANCHER_PROMETHEUS_STACK restart --batch-size 1 $RANCHER_PROMETHEUS_SERVICE
+if [ -n "$CATTLE_CONFIG_URL" ]; then
+  rancher-compose --url $CATTLE_CONFIG_URL \
+    --access-key $CATTLE_ACCESS_KEY --secret-key $CATTLE_SECRET_KEY \
+    -p $RANCHER_PROMETHEUS_STACK restart --batch-size 1 $RANCHER_PROMETHEUS_SERVICE
+fi
